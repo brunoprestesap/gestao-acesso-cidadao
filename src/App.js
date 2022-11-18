ee55a72c5713dca7c07019c7ac0e095cec76964b
Bruno Prestes <brunoasprestes@gmail.com>
2022-11-18 16:04:12 -0300

    component NavBar criado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import GetAllAccess from './components/GetAllAccess';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
 
-      <GetAllAccess />
+      <NavBar />
 
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from '../assets/download.png'
+
+function NavBar(){
+    return (
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            GateControl
+          </Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link>Home</Nav.Link>
+                <Nav.Link>Cadastro</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default NavBar;
