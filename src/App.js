6a41db93cedba893dc48c7081d8f3ae2747348b6
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-11-18 17:37:54 -0300

    incluida homepage.js e sua Rota

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+//
+/* import { Toaster } from 'react-hot-toast'; */
+import { Routes, Route } from 'react-router-dom';
+
+//
 
 function App() {
   return (
     <div className="App">
-
+      {/* <Toaster /> */}
       <NavBar />
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/NovoAcesso/:userID" element={<NovoAcesso />} /> */}
+      </Routes>
     </div>
   );
 }
